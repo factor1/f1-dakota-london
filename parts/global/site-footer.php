9c35011bfbdd717c4eb8562411d2ba1c06e6a6bf
Jorge <jorgeparraandrade@gmail.com>
2021-03-19 13:16:15 -0400

    Footer styles

--- parts/global/site-footer.php
+++ parts/global/site-footer.php
@@ -4,47 +4,43 @@
  */
 
 // Site Footer Custom Fields
-$logo = wp_get_attachment_image_src(get_field('footer_logo', 'option'), 'logo'); ?>
+$logo = wp_get_attachment_image_src(get_field('footer_logo', 'option'), 'medium');
+$content = get_field('footer_content', 'option');
+?>
 
 <footer class="site-footer">
   <div class="site-footer__top">
     <div class="container">
       <div class="row">
-        <div class="col-12">
 
+        <div class="col-12">
           <?php // Logo ?>
-          <div class="site-footer__logo sm-text-center">
+          <div class="site-footer__logo text-center">
             <a href="<?php echo esc_url(home_url()); ?>">
               <img src="<?php echo $logo[0]; ?>" alt="<?php echo get_bloginfo('name'); ?>">
             </a>
           </div>
+        </div>
 
+        <div class="col-12">
           <?php // Social menu
           prelude_social_menu(); ?>
-
         </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="site-footer__main">
-    <div class="container">
-      <div class="row">
+        <?php if($content): ?>
+          <div class="col-12">
+            <div class="site-footer__content">
+              <?php echo $content; ?>
+            </div>
+          </div>
+        <?php endif; ?>
 
-        <?php // Footer menu ?>
-        <div class="col-12 sm-text-center">
-
-          <?php wp_nav_menu(
-            array(
-              'theme_location' => 'footer',
-              'container' => 'nav',
-              'container_class' => 'nav--footer'
-            )
-          ); ?>
-
+        <div class="col-12 site-footer__copyright text-center">
+          <p>Copyright ©<?php echo Date('Y'); ?> Dakota London. All Rights Reserved. Site by factor1</p>
         </div>
 
       </div>
     </div>
   </div>
+
 </footer>
